28f3b0b9362e56c83886919791cc905b716a2976
Lolipop <127004286+FirmanBell@users.noreply.github.com>
2026-06-09 15:58:44 +0000

    Revert "cpuset: Tune cpu-assits for better performance and efficiency"
    This reverts commit 3880e77aec5e712503e2fb3d1c1332b4131d8461.

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -1796,13 +1796,13 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 {
 #ifdef CONFIG_CPUSET_ASSIST
 	static struct cs_target cs_targets[] = {
-		/* Little-only cpusets go first */
-		{ "foreground",		"0-3" },
-		{ "background",		"6-7" },
-		{ "system-background",	"6-7" },
-		{ "restricted",		"6-7" },
-		{ "top-app",		"0-3" },
-		{ "camera-daemon",	"0-7" },
+		{ "audio-app",		CONFIG_CPUSET_AUDIO_APP },
+		{ "background",		CONFIG_CPUSET_BG },
+		{ "camera-daemon",	CONFIG_CPUSET_CAMERA },
+		{ "foreground",		CONFIG_CPUSET_FG },
+		{ "restricted",		CONFIG_CPUSET_RESTRICTED },
+		{ "system-background",	CONFIG_CPUSET_SYSTEM_BG },
+		{ "top-app",		CONFIG_CPUSET_TOP_APP },
 	};
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
